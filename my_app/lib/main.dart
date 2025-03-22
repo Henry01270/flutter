2c238a74b4ac3e99de98ce0e6ab9497f83fb468e
Henry01270 <118987076+Henry01270@users.noreply.github.com>
2025-03-22 13:35:37 +0000

    25_0322_01

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -6,12 +6,17 @@ void main() {
   runApp(
     ///MaterialApp은 항상 최상위에 위치한다.
     /// Scaffold는 바로 아래에 위치한다.
-
+    /// 위젯 -> Widget => 화면에 무언가를 보여주게 하는 요소
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: Center(
           child: Text(
-          'Hello, World!'
+            'Hello, World!',
+            style: TextStyle(
+              color: Colors.white,
+            ), // textStyle
           ), // Text
         ), // Center
       ), // Scaffold
